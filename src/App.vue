8c1cd21b65df142c565ea9a94767f0731b8e6ef3
ganyuke <personalemailforedits@gmail.com>
2024-01-14 18:33:32 -0800

    Multiple songs testing

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,38 @@ import { ref } from 'vue'
 
 const playbackTime = ref(0) // in seconds
 const duration = ref(0) // in seconds
-let cancelId: number | null = null
+let audio: HTMLAudioElement | null
+let songIndex = 0
 
 const playAudio = async () => {
-  let sound = await import('@/assets/audio/Fontaine.flac')
-  const audio = new Audio(sound.default)
+  // exit early if audio already exists
+  if (audio) {
+    return
+  }
+
+  audio = new Audio(audioUrls[songMetadata.value.audioUrl])
+
   audio.onloadedmetadata = () => {
-    duration.value = audio.duration
+    if (audio) duration.value = audio.duration
   }
 
-  audio.onplaying = () => {
-    if (cancelId) clearInterval(cancelId)
-    playbackTime.value = audio.currentTime
-    cancelId = setInterval(() => playbackTime.value++, 1000)
+  audio.ontimeupdate = () => {
+    if (audio) playbackTime.value = audio.currentTime
   }
-
-  audio.onpause = () => {
-    if (cancelId) clearInterval(cancelId)
-  }
-
-  /*audio.ontimeupdate = () => {
-    playbackTime.value = audio.currentTime
-  }*/
 
   audio.play()
+
+  audio.onended = () => {
+    if (audio) audio.remove()
+    audio = null
+  }
+}
+
+const nextSong = async () => {
+  if (audio) audio = null
+  if (songIndex + 1 < songList.length) songIndex++
+  songMetadata.value = songList[songIndex]
+  console.log(songIndex, songMetadata.value)
 }
 
 const asTimestamp = (time: number) => {
@@ -42,14 +50,17 @@ const asTimestamp = (time: number) => {
 export default {
   methods: {
     playAudio,
-    asTimestamp
+    asTimestamp,
+    nextSong
   },
   setup() {
     return {
       playbackTime,
       duration,
       songMetadata,
-      quizMetadata
+      quizMetadata,
+      nextSongSeconds: ref(0),
+      coverImages
     }
   }
 }
@@ -60,23 +71,52 @@ type quizMeta = {
   playerCount: number
 }
 
-const quizMetadata: quizMeta = {
-  title: `Quiz Name 1 - Fontaine's Waltz & Tangos'`,
-  elapsedTime: 100,
-  playerCount: 10
-}
+const quizList: quizMeta[] = [
+  { title: `Quiz Name 1 - Fontaine's Waltz & Tangos`, elapsedTime: 100, playerCount: 10 }
+]
+let quizMetadata: quizMeta = quizList[0]
 
 type songMeta = {
   albumTitle?: string
+  coverArt: string
   artist: string
   songTitle: string
+  audioUrl: string
 }
 
-const songMetadata: songMeta = {
-  artist: 'HOYO-MiX',
-  songTitle: 'Fontaine',
-  albumTitle: 'Fountain of Bellau'
-}
+const songList: songMeta[] = [
+  {
+    artist: 'HOYO-MiX',
+    songTitle: 'Fontaine',
+    albumTitle: 'Fountain of Belleau',
+    coverArt: 'cover.jpg',
+    audioUrl: 'Fontaine.flac'
+  },
+  {
+    artist: 'HOYO-MiX',
+    songTitle: 'Le Souvenir avec le crepuscule',
+    albumTitle: 'Fountain of Belleau',
+    coverArt: 'cover.jpg',
+    audioUrl: 'Le Souvenir avec le crepuscule.flac'
+  }
+]
+let songMetadata = ref(songList[songIndex])
+
+// https://www.lichter.io/articles/nuxt3-vue3-dynamic-images/#the-importmetaglob-trick
+const covers: { [key: string]: { default: string } } = import.meta.glob(
+  '@/assets/images/covers/*.jpg',
+  { eager: true }
+)
+const coverImages = Object.fromEntries(
+  Object.entries(covers).map(([key, value]) => [key.split('/').pop(), value.default])
+)
+
+const audios: { [key: string]: { default: string } } = import.meta.glob('@/assets/audio/*.flac', {
+  eager: true
+})
+const audioUrls = Object.fromEntries(
+  Object.entries(audios).map(([key, value]) => [key.split('/').pop(), value.default])
+)
 </script>
 
 <template>
@@ -91,12 +131,13 @@ const songMetadata: songMeta = {
     <div class="grid sm:grid-cols-2 gap-4">
       <div>
         <div class="from-[#7dcefc] to-[#8dbc85] bg-gradient-to-b rounded-lg p-4" @click="playAudio">
-          <img src="./assets/images/cover.jpg" class="w-full" />
+          <img v-bind:src="coverImages[songMetadata.coverArt]" class="w-full" />
         </div>
-        <div class="bg-brand-card rounded-lg mt-4 p-4">
-          <img src="./assets/images/6638b0bc-b049-4399-96b0-a728e6e63761.png" class="bg-cover" />
-
-          <progress v-bind:max="duration" v-bind:value="playbackTime"></progress>
+        <div class="bg-brand-card rounded-lg mt-4 p-4 relative">
+          <label for="waveform" class="text-gray-400 absolute bottom-2"
+            >{{ asTimestamp(playbackTime) }} / {{ asTimestamp(duration) }}</label
+          >
+          <progress id="waveform" v-bind:max="duration" v-bind:value="playbackTime"></progress>
         </div>
       </div>
       <div class="bg-brand-card rounded-lg">
@@ -115,25 +156,14 @@ const songMetadata: songMeta = {
         <div class="text-center font-light my-3">
           <h2 class="font-black text-3xl text-center text-white my-3">Song information</h2>
           <p class="text-5xl" v-text="songMetadata.songTitle" id="song-title"></p>
-          <p class="text-lg" v-text="songMetadata.artist" id="artist"></p>
+          <p class="text-lg mt-2" v-text="songMetadata.artist" id="artist"></p>
           <p class="text-gray-400" v-bind:hidden="songMetadata.albumTitle === undefined">from</p>
           <p class="text-2xl italic" v-text="songMetadata.albumTitle" id="album"></p>
-          <!-- <progress id="fprogress {
-  appearance: none;
-  background: url('/src/assets/images/mask.png') #cccccc !important;
-}
-
-progress:after {
-  width: 100%;
-  background-color: aliceblue;
-  height: 100%;
-  /* mask-image: url('/src/assets/images/mask.png'); */
-}ile" v-bind:max="duration" v-bind:value="playbackTime">
-            {{ playbackTime }}%
-          </progress> -->
-
-          <!-- {{ asTimestamp(playbackTime) }} / {{ asTimestamp(duration) }} -->
         </div>
+      </div>
+      <div><!-- offset column --></div>
+      <div class="ml-auto mx-5" @click="nextSong">
+        <p class="text-gray-400">Next song in {{ nextSongSeconds }} second(s)</p>
       </div>
     </div>
   </div>
@@ -145,23 +175,22 @@ progress:after {
   content: '"';
 }
 
-/*
 progress {
   appearance: none;
-  background-image: url('/src/assets/images/mask.png');
+  background-image: url('@/assets/images/mask.png');
   background-color: transparent;
-  background-size: cover;
+  background-size: contain;
   background-repeat: no-repeat;
-  width: 100%;
+  width: 360px;
   height: 3em;
 }
 
+/* TODO: Fix this on Webkit/Blink & make not hardcoded */
 progress::-moz-progress-bar {
-  width: 100%;
-  height: 3em;
-  background-image: url('/src/assets/images/6638b0bc-b049-4399-96b0-a728e6e63761.png');
+  background-image: url('@/assets/images/6638b0bc-b049-4399-96b0-a728e6e63761.png');
   background-color: transparent;
-  background-size: cover;
+  background-size: 360px 100%;
+  background-position: 0px -2px;
   background-repeat: no-repeat;
-}*/
+}
 </style>
